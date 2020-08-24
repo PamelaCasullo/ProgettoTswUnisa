6ede2847edd63ace275e498916d93992bbfddeeb
rayleighRe1 <giuliotriggiani1@libero.it>
2020-08-24 19:22:14 +0200

    Revert "SigninDAO"
    This reverts commit 9759af4aa61d7a4c56119ed02fd4f81661c662af.

--- src/it/MadDiscord/Database/LoginDAO.java
+++ src/it/MadDiscord/Database/LoginDAO.java
@@ -16,8 +16,12 @@ public class LoginDAO {
 		UtenteBean logBean = null;
 		try (Connection conn = DBConnectionPool.getConnection(); )
 		{		
+
+			
 			//eseguiamo la query
+			
 			PreparedStatement prepStat = conn.prepareStatement("Select nome_utente, password_utente from UserTable where nome_utente =? and password_utente=?");
+			
 			prepStat.setString(1, utenteBean.getNome_utente());
 			prepStat.setString(2, utenteBean.getPassword_utente());
 			
@@ -39,8 +43,7 @@ public class LoginDAO {
 		return logBean;
 		
 	}
-	
-	
+
 	private void printSQLException(SQLException e) {
 
 		System.out.println(e);
